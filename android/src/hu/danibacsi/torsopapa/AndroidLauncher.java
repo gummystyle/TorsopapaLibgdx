6a1fde0f10c2cc065c159d1a73c4eadfa982fe46
gummystyle <birkasd73@gmail.com>
2016-03-06 19:14:29 +0100

    inital commit

---
+++ android/src/hu/danibacsi/torsopapa/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package hu.danibacsi.torsopapa;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import hu.danibacsi.torsopapa.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
